a167066231e7e271d715fa6e0f19205360909db9
ranggaju <ranggajzp@gmail.com>
2025-09-03 02:24:49 +0700

    update

--- module-01/day-03/exercise.js
+++ module-01/day-03/exercise.js
@@ -31,8 +31,96 @@ function fxCurFrmt(number) {
 
 console.log(fxCurFrmt(nCur));
 
-let sOrg = 'Hello World Hello';
-let sOrgSrch = 'Hello';
-let sOrgRmv = sOrg.split(sOrgSrch).join('').trim();
+let sOrg = 'Hello world, hello universe';
+let sOrgSrch = 'hello';
 
-console.log(`${sOrg} -> ${sOrgRmv}`);
+function fxRmvFrst(sStc, sSrch) {
+  let nIdx = sStc.indexOf(sSrch);
+  if (nIdx != -1) {
+    return sStc.slice(0, nIdx) + sStc.slice(nIdx + sSrch.length).trim();
+  }
+  return sStc.trim();
+}
+
+console.log(fxRmvFrst(sOrg, sOrgSrch));
+
+function fxKplFrst(sStc) {
+  let aStc = sStc.split(' ');
+  for (let i = 0; i < aStc.length; i++) {
+    aStc[i] = aStc[i].charAt(0).toUpperCase() + aStc[i].slice(1);
+  }
+  return aStc.join(' ');
+}
+
+console.log(fxKplFrst(sOrg));
+
+function fxSwpCs(sStc) {
+  let sSwp = '';
+  for (let i = 0; i < sStc.length; i++) {
+    let sChr = sStc.charAt(i);
+    if (sChr == sChr.toUpperCase()) {
+      sSwp += sChr.toLowerCase();
+    } else {
+      sSwp += sChr.toUpperCase();
+    }
+  }
+  return sSwp;
+}
+
+console.log(fxSwpCs(sOrg));
+
+let nNumA = 42;
+let nNumB = 27;
+let nNumC = 18;
+
+function fxMaxArr(aNum) {
+  let nMax = aNum[0];
+  for (let i = 1; i < aNum.length; i++) {
+    if (aNum[i] > nMax) {
+      nMax = aNum[i];
+    }
+  }
+  return nMax;
+}
+
+console.log(fxMaxArr([nNumA, nNumB, nNumC]));
+
+function fxSrtArr(aNum) {
+  for (let i = 0; i < aNum.length - 1; i++) {
+    for (let j = i + 1; j < aNum.length; j++) {
+      if (aNum[i] > aNum[j]) {
+        let nTmp = aNum[i];
+        aNum[i] = aNum[j];
+        aNum[j] = nTmp;
+      }
+    }
+  }
+  return aNum;
+}
+
+console.log(fxSrtArr([nNumA, nNumB, nNumC]));
+
+let anyInp = 'true';
+
+function fxCekTpe(anyVal) {
+  let nVal = (typeof anyInp == 'string') * 1 + (typeof anyInp == 'number') * 2;
+  nVal += (nVal == 0) * 3;
+  return nVal;
+}
+
+console.log(fxCekTpe(anyInp));
+
+let sInp = 'An apple a day keeps the doctor away';
+let sHd = 'a';
+
+function fxHdnChr(sStc, sHdnChr) {
+  let aStc = sStc.split('');
+  for (let i = 0; i < aStc.length; i++) {
+    if (aStc[i].toLowerCase() == sHdnChr.toLowerCase()) {
+      aStc[i] = '*';
+    }
+  }
+  return aStc.join('');
+}
+
+console.log(fxHdnChr(sInp, sHd));
