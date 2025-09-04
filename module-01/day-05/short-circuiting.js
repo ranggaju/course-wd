ab4a05a1643a961c45e6b6e7abd29aa1efd99db9
ranggaju <ranggajzp@gmail.com>
2025-09-04 16:03:39 +0700

    update

---
+++ module-01/day-05/short-circuiting.js
@@ -0,0 +1,17 @@
+// jika, true = 1, false = 0
+
+// sc && sama dengan dikalikan
+
+console.log(true * true); // 1
+console.log(true * false); // 0
+console.log(false * true); // 0
+console.log(false * false); // 0
+
+// sc || sama dengan dijumlahkan
+
+console.log(true + true); // 2
+console.log(true + false); // 1
+console.log(false + true); // 1
+console.log(false + false); // 0
+
+// asalkan hasilanya bukan 0, maka dianggap true
