9b51b60dd8536699f80d084dbf5a61cb756bf452
ranggaju <ranggajzp@gmail.com>
2025-09-07 14:54:01 +0700

    update content

--- module-01/day-05/exercise.js
+++ module-01/day-05/exercise.js
@@ -1,134 +1,220 @@
-function h1(string) {
-  const pad = ' '.repeat(Math.floor((30 - ('' + string).length) / 2));
-  const line = '='.repeat(30);
-  return console.log(`${line}\n\n${pad}${string}\n\n${line}`);
-}
+const linerLen = 50;
+const liner = (char = '') => char.repeat(linerLen);
+const padding = (text = '') => ' '.repeat(Math.floor((linerLen - text.length) / 2));
 
-function h2(string) {
-  const pad = ' '.repeat(Math.floor((30 - ('' + string).length) / 2));
-  const line = '-'.repeat(30);
-  return console.log(`\n${pad}${string}\n\n${line}`);
-}
+const hd = (text, level = 1) => {
+  const header = `- ${text} -`;
+  const pad = padding(header);
+  const border = level == 1 ? '=' : '-';
+  const top = level == 1 ? `${liner(border)}\n\n` : '';
+  const bot = level == 1 ? `\n\n${liner(border)}` : level == 2 ? `\n${liner(border)}` : '';
 
-function line1() {
-  console.log('='.repeat(30));
-}
+  console.log(`${top}${pad}${header}${bot}`);
+};
 
-function line2() {
-  console.log('-'.repeat(30));
-}
+const bl = (char) => console.log(liner(char));
 
-h1('Exercise Day 05');
-h2('First Page');
+hd('Exercise Day 05');
+hd('First Page', 2);
+
+hd('01', 3);
 
 // Write a function to get the lowest, highest and average value in the array (with and without sort function)
 
-function lha(...arr) {
-  let min = arr[0];
-  let max = arr[0];
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
-    }
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-    sum += arr[i];
-  }
-  const avr = Math.round((sum / arr.length) * 10000) / 10000;
-  return `lowest: ${min}, highest: ${max}, average: ${avr}`;
-}
+const minMaxAvr = (...numbers) => {
+  const min = Math.min(...numbers);
+  const max = Math.max(...numbers);
+  const sum = numbers.reduce((acc, n) => acc + n, 0);
+  const average = Math.round((sum / numbers.length) * 10000) / 10000;
 
-console.log(lha(12, 5, 23, 18, 4, 45, 32));
+  return `lowest: ${min}, highest: ${max}, average: ${average}`;
+};
 
-line2();
+console.log(minMaxAvr(12, 5, 23, 18, 4, 45, 32));
+
+hd('02', 3);
 
 // Write a function that takes an array of words and returns a string by concatenating the words in the array separated by commas and - the last word - by an 'and'
 
-function join(...arr) {
-  arr[arr.length - 1] = `and ${arr[arr.length - 1]}`;
-  return arr.join(', ');
-}
+const concatArr = (...array) => {
+  array[array.length - 1] = `and ${array[array.length - 1]}`;
 
-console.log(join('apple', 'banana', 'cherry', 'date'));
+  return array.join(', ');
+};
 
-line2();
+console.log(concatArr('apple', 'banana', 'cherry', 'date'));
+
+hd('03', 3);
 
 // Write a function from a given array of numbers and return the second smallest number
 
-// Mark 01
-function sml2Num(...arr) {
-  return arr.sort()[1];
-}
+const secondSmallest = (...array) => array.sort()[1];
 
-console.log(sml2Num(5, 3, 1, 7, 2, 6));
+console.log(secondSmallest(5, 3, 1, 7, 2, 6));
 
-// Mark 02 (Ignore duplicate)
-
-function sml2NumMk2(...arr) {
-  const dup = [];
-  for (let i = 1; i < arr.length; i++) {
-    let tmp = 0;
-    for (let j = 1; j < arr.length; j++) {
-      if (arr[j] < arr[j - 1]) {
-        tmp = arr[j - 1];
-        arr[j - 1] = arr[j];
-        arr[j] = tmp;
-      }
-    }
-  }
-  for (let i = 0; i < arr.length; i++) {
-    if (dup.includes(arr[i])) continue;
-    dup.push(arr[i]);
-  }
-  return dup[1];
-}
-
-console.log(sml2NumMk2(5, 3, 1, 7, 2, 6, 1));
-
-line2();
+hd('04', 3);
 
 // Write a function to calculate each element in the same position from two arrays of integer
 
-function add2Arr(arrA, arrB) {
+const addArr = (arrayA = [], arrayB = []) => arrayA.map((num, i) => num + arrayB[i]);
+
+console.log(addArr([1, 2, 3], [3, 2, 1]));
+
+hd('05', 3);
+
+// Write a function that adds an element to the end of an array. However, the element should only be added if it is not already in the array
+
+const addElement = (element, ...array) => (array.includes(element) ? array : [...array, element]);
+
+console.log(addElement(4, 1, 2, 3, 4));
+console.log(addElement(7, 1, 2, 3, 4));
+
+bl('=');
+hd('Second Page', 2);
+
+hd('01', 3);
+
+// Write a function from a given array of mixed data types and return the sum of all the number
+
+const sumOfNumber = (...array) => {
+  let add = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    typeof array[i] === 'number' && (add += array[i]);
+  }
+
+  return add;
+};
+
+console.log(sumOfNumber('3', 1, 'string', null, false, undefined, 2));
+
+hd('02', 3);
+
+// Write a function to insert multiple given integer (not an array) to an array and have a maximum size input. The array can only have a maximum size from a given input
+
+const maxSize = (number, ...array) => {
+  const len = number < array.length ? number : array.length;
+  const max = [];
+
+  for (let i = 0; i < len; i++) {
+    max.push(array[i]);
+  }
+
+  return max;
+};
+
+console.log(maxSize(5, 5, 10, 24, 3, 6, 7, 8));
+
+hd('03', 3);
+
+// Write a function that will combine 2 given array into one array
+
+const combineArray = (arrayA, arrayB) => [...arrayA, ...arrayB];
+
+console.log(combineArray([1, 2, 3], [4, 5, 6]));
+
+hd('04', 3);
+
+// Write a function to find duplicate values in an array
+
+const findDup = (...array) => {
+  const temp = [];
   const res = [];
-  for (let i = 0; i < arrA.length; i++) {
-    res.push(arrA[i] + arrB[i]);
+
+  for (let i of array) {
+    temp.includes(i) && !res.includes(i) && res.push(i);
+    !temp.includes(i) && temp.push(i);
   }
   return res;
-}
+};
 
-console.log(add2Arr([1, 2, 3], [3, 2, 1]));
+console.log(findDup(1, 2, 2, 2, 3, 3, 4, 5, 5));
 
-line1();
+hd('05', 3);
 
-h2('Second Page');
+// Write a function to find the difference in 2 given array
 
-h2('Third Page');
+const findDiff = (arrayA, arrayB) => {
+  const res = [];
+  for (let i of arrayA) {
+    !arrayB.includes(i) && !res.includes(i) && res.push(i);
+  }
+  for (let i of arrayB) {
+    !arrayA.includes(i) && !res.includes(i) && res.push(i);
+  }
+  return res;
+};
+
+console.log(findDiff([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]));
+
+bl('=');
+
+hd('Third Page', 2);
+hd('01', 3);
 
 // Based on the array below write a function that will return primitive data types only
 
+const primitiveOnly = (...array) => {
+  const res = [];
+  for (let n of array) {
+    typeof n != 'object' && res.push(n);
+  }
+  return res;
+};
+
+console.log(primitiveOnly(1, [], undefined, {}, 'string', {}, []));
+
+hd('02', 3);
+
 // Write a function from the below array of number that will return sum of duplicate values
 
-function sumDup(...arr) {
-  let sum = 0;
+const sumDup = (...array) => {
+  const temp = [];
   const dup = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (dup.includes(arr[i])) continue;
-    let count = 0;
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j] === arr[i]) {
-        count++;
-      }
-    }
-    if (count > 1) {
-      sum += arr[i] * count;
-      dup.push(arr[i]);
-    }
+  let res = 0;
+
+  for (let i of array) {
+    temp.includes(i) && !dup.includes(i) && dup.push(i);
+    !temp.includes(i) && temp.push(i);
   }
-  return sum;
-}
+
+  for (let i of array) {
+    dup.includes(i) && (res += i);
+  }
+
+  return res;
+};
 
 console.log(sumDup(10, 20, 40, 10, 50, 30, 10, 60, 10));
-line2();
+
+hd('03', 3);
+
+// Write a game of rock, paper, scissor function that will return 'Win' or 'Lose'
+
+const rockPaperScissor = (playerChoice = '') => {
+  playerChoice = playerChoice.toLowerCase();
+
+  const option = ['rock', 'paper', 'scissor'];
+  const computerChoice = option[Math.floor(Math.random() * option.length)];
+
+  if (!option.includes(playerChoice)) {
+    return `choose: ${option.slice(0, -1).join(', ')} or ${option.slice(-1)}`;
+  }
+
+  let message = `you throw ${playerChoice}, computer throw ${computerChoice}, you `;
+
+  message +=
+    playerChoice === computerChoice
+      ? 'draw'
+      : (playerChoice === option[0] && computerChoice === option[2]) ||
+        (playerChoice === option[1] && computerChoice === option[0]) ||
+        (playerChoice === option[2] && computerChoice === option[1])
+      ? 'win'
+      : 'lose';
+
+  return message;
+};
+
+console.log(rockPaperScissor('rock'));
+
+bl('=');
