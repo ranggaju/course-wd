2a6dfcdeb5ddfd485ce254902cef7bc457d07980
ranggaju <ranggajzp@gmail.com>
2025-10-10 19:09:39 +0700

    style: navbar

--- module-02/cc-personal-website/rj-personal-website/app/components/NavBar.tsx
+++ module-02/cc-personal-website/rj-personal-website/app/components/NavBar.tsx
@@ -1,12 +1,23 @@
+import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="sticky top-0 z-50 mx-4 mt-2 bg-white/30 backdrop-blur-md rounded-2xl p-6 shadow-lg">
+    <nav className="sticky top-0 z-50 mx-4 mt-2 p-6">
       <div className="flex justify-between items-center">
-        <div>Left</div>
-        <div>Mid</div>
-        <div>Right</div>
+        <div className="text-xl font-bold">
+          [<span className="text-orange-500">rj.</span>]
+        </div>
+        <div className="flex gap-6">
+          <Link href={"#about"}>About</Link>
+          <Link href={"#portofolio"}>Portofolio</Link>
+          <Link href={"#contact"}>Contact</Link>
+        </div>
+        <div>
+          <h1>
+            <span className="text-orange-500">Rangga</span> Julio Putra
+          </h1>
+        </div>
       </div>
     </nav>
   );
