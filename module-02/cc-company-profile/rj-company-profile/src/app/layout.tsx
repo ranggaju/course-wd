912123393c17f2ff19d1aad5edb9769748b710d4
ranggaju <ranggajzp@gmail.com>
2025-10-12 22:42:16 +0700

    feat: footer

--- module-02/cc-company-profile/rj-company-profile/src/app/layout.tsx
+++ module-02/cc-company-profile/rj-company-profile/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
