6a0181a8069fa6b1423b020c14c1da2fdf634540
ranggaju <ranggajzp@gmail.com>
2025-10-13 02:57:07 +0700

    fix: icon

--- module-02/cc-company-profile/rj-company-profile/src/app/layout.tsx
+++ module-02/cc-company-profile/rj-company-profile/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
